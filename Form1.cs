5be3dbbf97d6dac4b2a3db1f6b82127fc08c798e
andrew <unixerror@gmail.com>
2022-07-21 10:57:25 +0300

    add cell border

--- Form1.cs
+++ Form1.cs
@@ -75,7 +75,7 @@ namespace GameOfLife
                         newField[x, y] = field[x, y];
 
                     if (hasLife)
-                        graphics.FillRectangle(Brushes.Lime, x * resolution, y * resolution, resolution, resolution);
+                        graphics.FillRectangle(Brushes.Lime, x * resolution, y * resolution, resolution - 1, resolution - 1);
                 }
             }
             field = newField;
